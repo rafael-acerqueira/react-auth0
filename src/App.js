6814d77b76a2215521fb5f47a87604706f9065a8
Rafael <rafael.acerqueirah@gmail.com>
2019-09-23 11:28:10 -0300

    Create Navbar Component and add some styles

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from 'react'
 import { Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Profile from './pages/Profile'
+import Navbar from './components/Navbar'
 
 function App () {
 	return (
 		<>
-			<Route path="/" exact component={Home} />
-			<Route path="/profile" component={Profile} />
+			<Navbar />
+			<div className="body">
+				<Route path="/" exact component={Home} />
+				<Route path="/profile" component={Profile} />
+			</div>
 		</>
 	)
 }
