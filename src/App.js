3557cb2ea3cb1974cbb3f99f413aec2e22006f8b
Rafael <rafael.acerqueirah@gmail.com>
2019-09-23 11:20:38 -0300

    Setup routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React from 'react'
+import { Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Profile from './pages/Profile'
 
-function App() {
-  return <div className="App"></div>;
+function App () {
+	return (
+		<>
+			<Route path="/" exact component={Home} />
+			<Route path="/profile" component={Profile} />
+		</>
+	)
 }
 
-export default App;
+export default App
