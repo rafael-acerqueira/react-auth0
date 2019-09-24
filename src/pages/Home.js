a768c4c55d3dc7eee725854d15fc354833d0772d
Rafael <rafael.acerqueirah@gmail.com>
2019-09-24 10:30:38 -0300

    Show login button or menu

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = ({ auth }) => (
 	<div>
 		<h1>Home</h1>
-		<button onClick={auth.login}>Login</button>
+		{auth.isAuthenticated() ? (
+			<Link to="/profile">View Profile</Link>
+		) : (
+			<button onClick={auth.login}>Login</button>
+		)}
 	</div>
 )
 
