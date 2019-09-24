4cad97494a087c500374845da7cd99cbb414cf90
Rafael <rafael.acerqueirah@gmail.com>
2019-09-24 09:32:19 -0300

    Create class to configure Auth0

---
+++ src/Auth/Auth.js
@@ -0,0 +1,18 @@
+import auth0 from 'auth0-js'
+
+export default class Auth {
+	constructor (history) {
+		this.history = history
+		this.auth0 = new auth0.WebAuth({
+			domain: process.env.REACT_APP_AUTH0_DOMAIN,
+			clientId: process.env.REACT_APP_AUTH0_CLIENT_ID,
+			redirectUri: process.env.REACT_APP_AUTH0_CALLBACK_URL,
+			responseType: 'token id_token',
+			scope: 'openid profile email'
+		})
+	}
+
+	login = () => {
+		auth0.authorize()
+	}
+}
