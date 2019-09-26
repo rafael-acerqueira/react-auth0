f54951219761b2e43aa1fc6400fb150cb9aa4dec
Rafael <rafael.acerqueirah@gmail.com>
2019-09-26 17:24:31 -0300

    Create private endpoint

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
 const express = require('express')
+const jwt = require('express-jwt')
+const jwksRsa = require('jwks-rsa')
 require('dotenv').config()
+
+const checkJwt = jwt({
+	secret: jwksRsa.expressJwtSecret({
+		cache: true,
+		rateLimit: true,
+		jwksRequestsPerMinute: 5,
+		jwksUri: `https://${process.env.REACT_APP_AUTH0_DOMAIN}/.well-known/jwks.json`
+	}),
+
+	audience: process.env.REACT_APP_AUTH0_AUDIENCE,
+	issuer: `https://${process.env.REACT_APP_AUTH0_DOMAIN}`,
+
+	algorithms: ['RS256']
+})
 
 const app = express()
 
 app.get('/public', (req, res) => {
 	res.json({ message: 'Hello from public api' })
+})
+
+app.get('/private', checkJwt, (req, res) => {
+	res.json({ message: 'Hello from private api' })
 })
 
 app.listen(3001, (req, res) => {
